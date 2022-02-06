e0a71871291ca61627be72d75a1ec6a928515962
shinp09 <57585657+shinp09@users.noreply.github.com>
2022-02-06 15:02:00 +0900

    feat: request

--- Jirachi/main.go
+++ Jirachi/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello")
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("読み込み出来ませんでした: %v", err)
+	}
+	user := os.Getenv("USER_NAME")
+	password := os.Getenv("PASSWORD")
+	requestUrl := os.Getenv("BASE_URL")
+
+	req, err := http.NewRequest(http.MethodGet, requestUrl, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// 認証情報を付与し、リクエストと一緒に送る
+	req.SetBasicAuth(user, password)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// レスポンスボディをすべて読み出す
+	body, _ := ioutil.ReadAll(resp.Body)
+	// body は []byte
+	fmt.Println(string(body))
 }
